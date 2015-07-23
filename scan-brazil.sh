84fb2204c68e461febef5df28fef62f8e936efc7
lanc33 <lancee@yandex.com>
2015-07-23 18:24:43 -0300

    braZil

--- scan-brazil.sh
+++ scan-brazil.sh
@@ -3,5 +3,5 @@
 # usage: sudo ./scan-brazil.sh
 # will scan brazil for port 5900
 masscan --conf brazil.conf
-awk '/^Host:/ { print $2 }' brasil-results > brasil-results-awked
-nmap --script open-curtains.nse -n -Pn -p 5900 -iL brasil-results-awked
+awk '/^Host:/ { print $2 }' brazil-results > brazil-results-awked
+nmap --script open-curtains.nse -n -Pn -p 5900 -iL brazil-results-awked
